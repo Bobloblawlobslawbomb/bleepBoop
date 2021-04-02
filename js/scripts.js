af68035222e200ead80f15741d270b380eeec67b
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-02 11:37:48 -0700

    WIP: reviewing weeks material--I need better notes

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,29 @@ const beep = "Beep!"
 const boop = "Boop!"
 const beMy = "Won't you be my neighbor?"
 const botArray = ["Beep!", "Boop!", "Won't you be my neighbor?"]
-const userArray = []
-const amendedArray = []
-
+let userArray = []
+let amendedArray = []
 
 $(document).ready(function () {
   $("#formOne").submit(function (e) {
     e.preventDefault();
     const personInput = parseInt($('input#userNum').val())
   });
+
+  $(".refresh").click(function () {
+    location.reload()
+  })
 });
 
+function beepBoop() {
+  for (let i = 0; i < personInput.length; i++) {
+    let userArray = [personInput]
+    return userArray
+  }
+}
 
-//const personInput = parseInt($('input#userNum').val())
-//answerOne = parseInt($("#radioOneDiv input[type='radio']:checked").val())
+
+
+
+
+
