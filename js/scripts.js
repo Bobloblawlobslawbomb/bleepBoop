a0b2b75a4ed771032e1fa15ca55a0e82865f9d4c
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-08 21:18:25 -0700

    WIP: output in correct order. still needs commas

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 function beepBoop(userNumber) {//still need to add "," --think we can do it at the 'else' and after "neighbor?" 
   let createdArray = []
   for (let index = 0; index <= userNumber; index += 1) {
-    if (index.toString().includes("1")) {
-      createdArray.push("Beep!,")
-    } else if (index.toString().includes("2")) {
-      createdArray.push("Boop!,")
-    } else if (index.toString().includes("3")) {
+    if (index.toString().includes("3")) {
       createdArray.push("Won't you be my neighbor?")
+    } else if (index.toString().includes("2")) {
+      createdArray.push("Boop!")
+    } else if (index.toString().includes("1")) {
+      createdArray.push("Beep!")
     } else {
       createdArray.push(index)
     }
